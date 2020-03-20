d8bac6996bf3913d1596498de67e738c0f240d0d
farruxxamidov <farruxxamidov@gmail.com>
2020-03-20 12:55:04 +0500

    feat: resolve task 'morse-decoder'

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    //split to words
+    let numArr = expr.split('**********');
+
+    //split to letters
+    for (let i=0; i<numArr.length; i++) {
+        numArr[i]=numArr[i].match(/.{1,10}/g);
+    }
+
+    const regex10 = /10/g;
+    const regex11 = /11/g;
+    const regex0 = /0/g;
+    
+    for (let i=0; i<numArr.length; i++) {
+        for (j=0; j<numArr[i].length; j++) {
+            numArr[i][j] = numArr[i][j].replace(regex10, '.');
+            numArr[i][j] = numArr[i][j].replace(regex11, '-');
+            numArr[i][j] = numArr[i][j].replace(regex0, '');
+        }
+    }
+
+    for (let i=0; i<numArr.length; i++) {
+        for (j=0; j<numArr[i].length; j++) {
+            numArr[i][j] = MORSE_TABLE[numArr[i][j]];
+        }
+    }
+
+    for (let i=0; i<numArr.length; i++) {
+        numArr[i] = numArr[i].join('');
+    }
+    //console.log(numArr.join(' '));
+    return numArr.join(' ');    
 }
 
 module.exports = {
